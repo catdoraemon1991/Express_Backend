e87825b6be71e65ca1404d22deedadd3537cd1de
catdoraemon1991 <catdoraemon1991@gmail.com>
2019-08-31 13:49:33 -0400

    db

--- Express/src/db/DBConnectionFactory.java
+++ Express/src/db/DBConnectionFactory.java
@@ -1,5 +1,27 @@
 package db;
 
 public class DBConnectionFactory {
+	// This should change based on the pipeline.
+			private static final String DEFAULT_DB = "firebase";
+			
+			public static DBConnection getConnection(String db) {
+				switch (db) {
+				case "firebase":
+					//return new FirebaseConnection();
+					return null;
+				case "mysql":
+					//return new MySQLConnection();
+					return null;
+				case "mongodb":
+					//return new MongoDBConnection();
+					return null;
+				default:
+					throw new IllegalArgumentException("Invalid db:" + db);
+				}
 
+			}
+
+			public static DBConnection getConnection() {
+				return getConnection(DEFAULT_DB);
+			}
 }
