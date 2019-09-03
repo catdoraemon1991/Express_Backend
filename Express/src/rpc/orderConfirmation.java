4b4cf60a242a92f9f6a498ed92d2d2da50f292dc
catdoraemon1991 <catdoraemon1991@gmail.com>
2019-09-02 21:55:08 -0400

    orderCofirmation

--- Express/src/rpc/orderConfirmation.java
+++ Express/src/rpc/orderConfirmation.java
@@ -1,11 +1,28 @@
 package rpc;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Machine;
+import entity.Order;
+import entity.Order.OrderBuilder;;
 
 /**
  * Servlet implementation class orderConfirmation
@@ -35,11 +52,119 @@ public class orderConfirmation extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// step 1: read JSON object from HTTP request 
+		JSONObject orderInfo = RpcHelper.readJSONObject(request);
+		JSONObject resJSON = new JSONObject();
 		// step 2: From the JSON object, read order information
-		// step 3: calculate departTime, pickupTime and deliveryTime from Google API
-		// step 4: write output from step 2&3 to a new Order class using builder pattern (see entity package)
-		// step 5: write the new Order into database using the saveOrder method in db/DBConnection.java
-		// step 6: set up timer for changing machine status using the updateStatus method in db/DBConnection.java
+		OrderBuilder newOrder = new OrderBuilder();
+		String stationId = "";
+		String type = "";
+		String userId = "";
+		String machineId = "";
+		String destination = "";
+		String shippingAddress = "";
+		Long shippingTime = null;
+		Long departTime = (long) 1234; //null;
+		Long pickupTime = (long) 5678910; //null;
+		Long deliveryTime = (long) 11121314;//null;
+		try {
+			if (! orderInfo.isNull("userId")) {
+				userId = orderInfo.getString("userId");
+			}
+			if (! orderInfo.isNull("shippingAddress")) {
+				shippingAddress = orderInfo.getString("shippingAddress");
+			}
+			if (! orderInfo.isNull("destination")) {
+				destination = orderInfo.getString("destination");
+			}
+			if (! orderInfo.isNull("shippingTime")) {
+				shippingTime = Long.valueOf(orderInfo.getString("shippingTime"));
+			}
+			if (! orderInfo.isNull("shippingMethod")) {
+				type = orderInfo.getString("shippingMethod");
+			}
+			if (! orderInfo.isNull("stationId")) {
+				stationId = orderInfo.getString("stationId");
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (userId == "") {
+			try {
+				resJSON.put("Error", "Wrong userId input");
+				RpcHelper.writeJsonObject(response, resJSON);
+				return;
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		// step 3: calculate robot availability, select a robot and update robot status
+		DBConnection db = DBConnectionFactory.getConnection();
+		List<Machine> machines = new ArrayList<>();
+		if (stationId != "" && type != "") {
+			machines = db.getMachineByType(db.getMachine(stationId), type);
+		}else {
+			try {
+				resJSON.put("Error", "Wrong shipping method input");
+				RpcHelper.writeJsonObject(response, resJSON);
+				return;
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+		if (machines.size() == 0) {
+			try {
+				resJSON.put("Back", "Machine not available, please re-select");
+				RpcHelper.writeJsonObject(response, resJSON);
+				return;
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			machineId = machines.get(0).getMachineId();
+			//db.machineOccupied(machineId);  //uncomment this		
+		}
+		// step 4: calculate departTime, pickupTime and deliveryTime from Google API
+		Calendar ship = Calendar.getInstance(TimeZone.getTimeZone("PST"));
+		ship.setTimeInMillis(shippingTime);
+		// step 5: write output from previous steps to a new Order class using builder pattern (see entity package)
+		newOrder.setUserId(userId);
+		newOrder.setShippingAdress(shippingAddress);
+		newOrder.setDestination(destination);
+		newOrder.setShippingTime(shippingTime);
+		newOrder.setShippingMethod(type);
+		newOrder.setMachineId(machineId);
+		newOrder.setDepartTime(departTime);
+		newOrder.setPickupTime(pickupTime);
+		newOrder.setDeliveryTime(deliveryTime);
+		Order order = newOrder.build();
+		// step 6: write the new Order into database using the saveOrder method in db/DBConnection.java
+		String newOrderId = "1234567";
+		//String newOrderId = db.saveOrder(userId, order); //uncomment this
+		
+		// step 7: return order Id & machine status	
+		if (newOrderId != "") {
+			try {
+				resJSON.put("orderId", newOrderId);
+				RpcHelper.writeJsonObject(response, resJSON);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		// step 8: set up timer for changing machine status using the updateStatus method in db/DBConnection.java
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+			public void run() {
+				//db.updateStatus(newOrderId, machineId);
+			}
+		};
+		//timer.schedule(task, date);
+
+		System.out.println(machineId);
 	}
 
 }
